14a9c25d7a54d749c2185ee391f6cc92870c0fb4
Ádrian Rabelo <adrianrabeloe10@gmail.com>
2022-10-14 00:20:52 -0400

    creating notification dialog

---
+++ front/src/utils/Notification.tsx
@@ -0,0 +1,13 @@
+import Swal from "sweetalert2";
+
+export const Notification = Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 4000,
+  timerProgressBar: true,
+  didOpen: (notification) => {
+    notification.addEventListener("mouseenter", Swal.stopTimer);
+    notification.addEventListener("mouseleave", Swal.resumeTimer);
+  },
+});
