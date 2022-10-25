4de2a28339a589dc1a07b4264c85d6bdf887f0d9
Ádrian Rabelo <adrianrabeloe10@gmail.com>
2022-10-25 09:45:53 -0400

    changing routes logic

--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -1,13 +1,14 @@
-import { Routes, Route } from 'react-router-dom'
-import { Login } from '../pages/login'
-import { SignUp } from '../pages/signup'
-import Home from './../index'
+import { Routes, Route } from "react-router-dom";
+import { Login } from "../pages/login";
+import { SignUp } from "../pages/signup";
+import Home from "./../index";
+import { CreateCostumer } from "../pages/modules/costumer/CreateCostumer";
 
 const Router: React.FC = () => (
   <Routes>
-    <Route path='/login' element={ <Login /> } />
-    <Route path='/temp' element={ <Home /> } />
-    <Route path='/signup' element={ <SignUp /> } />
+    <Route path="/" element={<Login />} />
+    <Route path="/temp" element={<Home />} />
+    <Route path="/signup" element={<SignUp />} />
   </Routes>
-)
-export default Router
+);
+export default Router;
--- front/src/routes/protectedRoute.tsx
+++ front/src/routes/protectedRoute.tsx
@@ -1,15 +1,18 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
 export const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
   const auth = useContext(AuthContext);
   const navigate = useNavigate();
+  const location = useLocation();
   const storagedToken = localStorage.getItem("@App:token");
   useEffect(() => {
-    if (!storagedToken) {
-      navigate("/login");
+    if (!storagedToken && location.pathname !== '/signup') {
+      navigate("/")
+    } else if (!storagedToken) {
+      navigate('/signup')
     } else {
       navigate("/temp");
     }
