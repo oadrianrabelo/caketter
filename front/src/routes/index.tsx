4ea71242a16f51ba41bd0ba2f570b19f917488e6
Ádrian Rabelo <adrianmendes@setic.ro.gov.br>
2022-10-27 21:35:14 -0400

    project imports

--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -2,13 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import { Login } from "../pages/login";
 import { SignUp } from "../pages/signup";
 import Home from "./../index";
-import { CreateCostumer } from "../pages/modules/costumer/CreateCostumer";
+import Costumers from '../pages/modules/costumer/Costumers';
+import { CreateCostumer } from '../pages/modules/costumer/CreateCostumer';
 
 const Router: React.FC = () => (
   <Routes>
     <Route path="/" element={<Login />} />
     <Route path="/temp" element={<Home />} />
     <Route path="/signup" element={<SignUp />} />
+    <Route path="/costumers" element={<Costumers />} />
+    <Route path="/costumer/create" element={<CreateCostumer />} />
   </Routes>
 );
 export default Router;
