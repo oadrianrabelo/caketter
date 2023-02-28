6e23a6c8a3d93705025adaea7d60a66a21c9a6b5
Ádrian Rabelo <adrianmendes@setic.ro.gov.br>
2023-02-28 18:06:42 -0400

    fix selected costumer

--- front/src/pages/modules/order/CreateOrder.tsx
+++ front/src/pages/modules/order/CreateOrder.tsx
@@ -80,7 +80,9 @@ export function CreateOrder() {
           id="select-costumer"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         >
-          <option disabled>Escolha um cliente</option>
+          <option disabled selected>
+            Escolha um cliente
+          </option>
           {costumers.map((costumer) => {
             return (
               <option key={costumer.id} value={costumer.id}>
