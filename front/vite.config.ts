ed0cfdde8ecd7ad4119b8b34a39412897b45ec36
Ádrian Rabelo <adrianmendes@setic.ro.gov.br>
2023-06-15 12:44:39 -0400

    enabling hot reload on docker

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, preview } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ include: "**/*.tsx"})],
+  base: './',
   server: {
-    host: true,
     port: 3000,
+    host: true,
+    hmr: true,
     watch: {
       usePolling: true,
-    }
-  }
+    },
+  },
 })
